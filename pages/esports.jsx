f5e9d9de0a863b2c2bcc8afed0e92856d0a64dad
stutterat <indtarun5@gmail.com>
2022-04-30 22:53:04 +0530

    added the gaming links route

---
+++ pages/esports.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head"
+
+
+function esports() {
+  return (
+    <div>
+        <div className="">
+            <Head>
+                <title>Gaming | TechnoHolix - 2K22</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div>
+                    <h1 className="text-center mt-9 mb-5 text-2xl font-extrabold">ESPORT EVENTS AT TECHNOHOLIX 2K22</h1>
+                    <p className="text-center mt-9 mb-5 font-bold">Click on the game you want to participate in.</p>
+                    <p className="text-center hover:cursor-pointer mt-9 mb-5 text-xl font-bold"><a href="https://forms.gle/crxVPz7up9NjgdwJ7 ">BGMI</a></p>
+                    <p className="text-center hover:cursor-pointer mt-9 mb-5 text-xl font-bold"><a href="https://forms.gle/kNFXJ1gJqx2GKhim6 ">CS 1.6</a></p>
+                    <p className="text-center hover:cursor-pointer mt-9 mb-5 text-xl font-bold"><a href="https://forms.gle/crxVPz7up9NjgdwJ7 ">NSF 2005</a></p>
+                        
+                    
+                </div>
+            </main>
+        </div>
+    </div>
+  )
+}
+
+export default esports
