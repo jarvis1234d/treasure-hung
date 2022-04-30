e356c7338511b8110da4d162fee9f02e206350b3
stutterat <indtarun5@gmail.com>
2022-04-30 10:51:37 +0530

    Add image support

--- pages/qr/[hash].jsx
+++ pages/qr/[hash].jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import md5 from "md5";
 import { useState } from "react";
 import {Oval} from "react-loading-icons";
+import Image from "next/image";
 
 function qr({msg}) {
 
@@ -27,9 +28,14 @@ function qr({msg}) {
             <p className="mt-5 text-[#06bcee]" >Verifying...</p>
           </div>
           <div className={`${loading && 'hidden'} flex items-center flex-col`}>
-            <p className="text-center text-white text-2xl font-extrabold">Your Clue: {msg.hint}</p>
-          <br /><br /><br />
-          <p className="text-center text-white text-lg font-extrabold ">QR Code Id: {msg.url}</p>
+            {
+              msg.hint !== null && <p className="text-center text-white text-2xl font-extrabold">Your Clue: {msg.hint}</p>
+            }
+            {
+              msg.img !== null && <div className="pl-2  pr-2 w-full"> <Image className="rounded "  src={msg.img} width={100} height={100} layout="responsive" objectFit="cover" /> </div>
+            }
+          
+          <p className="mt-9 text-center text-white text-lg font-extrabold ">QR Code Id: {msg.url}</p>
           </div>
       </main>
 
@@ -42,50 +48,61 @@ export const getServerSideProps = async(context) => {
   const activeLinks = [
     {
       url: "1",
-      hint: "This is hint for 1 🇮🇳"
+      hint: null,
+      img: "/images/Donut.png"
     },
     {
       url: "2",
-      hint: "This is hint for 2 🐼"
+      hint: "This is hint for 2 🐼",
+      img: null
     },
     {
       url: "3",
-      hint: "This is hint for 3 🐘"
+      hint: "This is hint for 3 🐘",
+      img: null
     },
     {
       url: "4",
-      hint: "This is hint for 4 🦏"
+      hint: "This is hint for 4 🦏",
+      img: null
     },
     {
       url: "5",
-      hint: "This is hint for 5 💔"
+      hint: "This is hint for 5 💔",
+      img: null
     },
     {
       url: "6",
-      hint: "This is hint for 6 🥑"
+      hint: null,
+      img: "/images/Autumn-leaves.jpg"
     },
     {
       url: "7",
-      hint: "This is hint for 7 🥑"
+      hint: "This is hint for 7 🥑",
+      img: null
     },
     {
       url: "8",
-      hint: "This is hint for 8 🥑"
+      hint: "This is hint for 8 🥑",
+      img: null
     },
     {
       url: "9",
-      hint: "This is hint for 9 🕵🏻‍♀️"
+      hint: "This is hint for 9 🕵🏻‍♀️",
+      img: null
     },
     {
       url: "10",
-      hint: "This is hint for 10 🐶"
+      hint: "This is hint for 10 🐶",
+      img: null
     },
   ]
 
     const {hash} = context.query;
     var msg ={
-      url: "blank",
-      hint: "blank"
+      url: null,
+      hint: null,
+      img: null
     };
     
     for(var i = 0; i < activeLinks.length; i++){
