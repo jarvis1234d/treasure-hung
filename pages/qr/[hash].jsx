c08b4095897a22273ec6d26baa96a356eccbf352
stutterat <indtarun5@gmail.com>
2022-05-04 15:23:11 +0530

    Ohh stree kal ana

--- pages/qr/[hash].jsx
+++ pages/qr/[hash].jsx
@@ -34,7 +34,7 @@ function qr({msg}) {
               msg.img !== null && <div className="pl-2  pr-2 w-full"> <Image className="rounded "  src={msg.img} width={100} height={100} priority={true} layout="responsive" objectFit="contain" /> </div>
             }
             {
-              msg.hint !== null && <p className="mt-5 ml-1 mr-1 text-center text-white text-2xl font-extrabold">Your Clue is: {msg.hint}</p>
+              msg.hint !== null && <p className="mt-5 ml-1 mr-1 text-center text-white text-2xl font-extrabold">{msg.hint}</p>
             }
 
             {
@@ -91,217 +91,217 @@ export const getServerSideProps = async(context) => {
       hint: null,
       img: null,
       key: "'T'",
-      _for: "alpha1, alpha2",
+      _for: "alpha1, alpha2,",
       audio: "/audio/sherayasher.wav"
     },
-    // {
-    //   url: "15",
-    //   hint: "This is hint for 15 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "16",
-    //   hint: "This is hint for 16 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "17",
-    //   hint: "This is hint for 17 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "18",
-    //   hint: "This is hint for 18 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "19",
-    //   hint: "This is hint for 19 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "20",
-    //   hint: "This is hint for 20 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "21",
-    //   hint: "This is hint for 21 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "22",
-    //   hint: "This is hint for 22 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "23",
-    //   hint: "This is hint for 23 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "24",
-    //   hint: "This is hint for 24 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "25",
-    //   hint: "This is hint for 25 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "26",
-    //   hint: "This is hint for 26 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "27",
-    //   hint: "This is hint for 27 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "28",
-    //   hint: "This is hint for 28 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "29",
-    //   hint: "This is hint for 29 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "30",
-    //   hint: "This is hint for 30 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "31",
-    //   hint: "This is hint for 30 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "32",
-    //   hint: "This is hint for 32 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "33",
-    //   hint: "This is hint for 33 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "34",
-    //   hint: "This is hint for 34 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "35",
-    //   hint: "This is hint for 35 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "36",
-    //   hint: "This is hint for 36 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "37",
-    //   hint: "This is hint for 37 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "38",
-    //   hint: "This is hint for 38 🐶",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "39",
-    //   hint: "Congratulations!!! You've successfully completed treasure hunt. Now go to the Anchors and submit your all keys in sequence",
-    //   img: "/images/winner.gif",
-    //   key: "'C'",
-    //   _for: null,
-    //   audio: null
-    // },
-    // {
-    //   url: "demo",
-    //   hint: "This is Demo page",
-    //   img: null,
-    //   key: null,
-    //   _for: null,
-    //   audio: null
-    // },
+    {
+      url: "15",
+      hint: "This is hint for 15 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "16",
+      hint: "This is hint for 16 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "17",
+      hint: "This is hint for 17 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "18",
+      hint: "This is hint for 18 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "19",
+      hint: "This is hint for 19 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "20",
+      hint: "This is hint for 20 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "21",
+      hint: "This is hint for 21 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "22",
+      hint: "This is hint for 22 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "23",
+      hint: "This is hint for 23 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "24",
+      hint: "This is hint for 24 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "25",
+      hint: "This is hint for 25 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "26",
+      hint: "This is hint for 26 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "27",
+      hint: "This is hint for 27 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "28",
+      hint: "This is hint for 28 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "29",
+      hint: "This is hint for 29 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "30",
+      hint: "This is hint for 30 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "31",
+      hint: "This is hint for 30 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "32",
+      hint: "This is hint for 32 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "33",
+      hint: "This is hint for 33 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "34",
+      hint: "This is hint for 34 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "35",
+      hint: "This is hint for 35 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "36",
+      hint: "This is hint for 36 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "37",
+      hint: "This is hint for 37 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "38",
+      hint: "This is hint for 38 🐶",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
+    {
+      url: "39",
+      hint: "Congratulations!!! You've successfully completed treasure hunt. Now go to the Anchors and submit your all keys in sequence",
+      img: "/images/winner.gif",
+      key: "'C'",
+      _for: null,
+      audio: null
+    },
+    {
+      url: "demo",
+      hint: "This is Demo page",
+      img: null,
+      key: null,
+      _for: null,
+      audio: null
+    },
 
   ]
 
