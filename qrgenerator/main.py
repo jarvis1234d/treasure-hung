7fd96464659959ff5097d60029f16619f74a982d
stutterat <indtarun5@gmail.com>
2022-05-09 23:18:23 +0530

    turn off all qrs

--- qrgenerator/main.py
+++ qrgenerator/main.py
@@ -7,7 +7,7 @@ import hashlib
 for i in range(11,40):
 	result = bytes(str(i), 'utf-8')
 	result = hashlib.md5(result)
-	data = 'https://treasure-hunt007.herokuapp.com/qr/' + result.hexdigest()
+	data = 'YOUR LINK HERE' + result.hexdigest()
  
 	# Encoding data using make() function
 	img = qrcode.make(data)
