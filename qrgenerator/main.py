ddc6af3689b03933c1bc323fe98712246adb1ac4
Tarun Suryawanshi <tarun@team.ovrcraft.com>
2022-08-29 12:05:51 +0530

    online

--- qrgenerator/main.py
+++ qrgenerator/main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-import qrcode
+# import qrcode
 import hashlib
 
 
-for i in range(11,40):
-	result = bytes(str(i), 'utf-8')
-	result = hashlib.md5(result)
-	data = 'YOUR LINK HERE' + result.hexdigest()
+# for i in range(11,40):
+# 	result = bytes(str(i), 'utf-8')
+# 	result = hashlib.md5(result)
+# 	data = 'YOUR LINK HERE' + result.hexdigest()
  
-	# Encoding data using make() function
-	img = qrcode.make(data)
+# 	# Encoding data using make() function
+# 	img = qrcode.make(data)
 	 
-	# Saving as an image file
-	img.save('My'+ str(i) + '.png')
+# 	# Saving as an image file
+# 	img.save('My'+ str(i) + '.png')
+
+print(hashlib.md5("demo"))
